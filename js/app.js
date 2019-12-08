76875b80435f22bc6f6f52b9058d157ebe6ba0fe
AdrienRitaine <adrien.ritaine@yahoo.com>
2019-12-08 12:49:18 +0100

    modification maps.js en POO + ajout Jquery pour la gestion de l API

--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
 // Ajout du slider
-let slider = new Slider();
+var slider = new Slider();
 // Ajout de la carte
-let maps = new Maps("9d52275b194f4eb0191ec9bb7f18ecb771942982", [49.8946874, 2.2953211], "Amiens", "map");
+var maps = new Maps("9d52275b194f4eb0191ec9bb7f18ecb771942982", [49.8946874, 2.2953211], "Amiens", "map");
+maps.addMap();
+maps.getAjax();
 // Ajout du canvas
-let canvas = new Canvas();
+var canvas = new Canvas();
--- js/map.js
+++ js/map.js
@@ -1,116 +1,13 @@
 // MAP //
 
-class Maps{
-	constructor(api_key, longLat, station, id){
+class Maps
+{
+	constructor(api_key, longLat, station, id)
+	{
 		this.longLat = longLat; // Coordonnées de la carte
 		this.api_key = api_key; // Clé pour l'API
 		this.id = id; // id de la map
 		this.station = station; // Station selectionnée
-		var map = L.map(this.id).setView(this.longLat, 14);
-
-		// Ajout de la carte
-		L.tileLayer('https://tiles.wmflabs.org/hikebike/{z}/{x}/{y}.png', {
-			attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-		}).addTo(map);
-
-		// Creation de la requête
-		var request = new XMLHttpRequest();
-		request.open('GET', 'https://api.jcdecaux.com/vls/v1/stations?contract=' + this.station + '&apiKey=' + this.api_key, true);
-		request.onload = function() {
-			
-			var data = JSON.parse(this.response);
-			var greenIcon = L.icon({
-				iconUrl: 'images/marker_icon_green.png',
-				iconSize:     [38, 48]
-			});
-			var orangeIcon = L.icon({
-				iconUrl: 'images/marker_icon_orange.png',
-				iconSize:     [38, 48]
-			});
-			var redIcon = L.icon({
-				iconUrl: 'images/marker_icon_red.png',
-				iconSize:     [38, 48]
-			});
-			
-			for (var i = 0; i < data.length; i++) {
-
-				// Variables
-				var latMarker = data[i].position.lat;
-				var longMarker = data[i].position.lng;
-				var longLatMarker = [latMarker, longMarker];
-				var veloPlace = data[i].bike_stands;
-				var veloAddr = data[i].address;
-				var veloDisp = data[i].available_bikes;
-				var station = data[i].name;
-				var status = data[i].status;
-
-				// Ajout marker
-				if (status === "OPEN") {
-					status = "OUVERT";
-					if (veloDisp >= 8) {
-						var marker = new L.Marker(longLatMarker, {
-							draggable: false,
-							icon: greenIcon
-						});
-					}
-					else if (veloDisp >= 5){
-						var marker = new L.Marker(longLatMarker, {
-							draggable: false,
-							icon: orangeIcon
-						});
-					}
-					else if (veloDisp <= 4){
-						var marker = new L.Marker(longLatMarker, {
-							draggable: false,
-							icon: redIcon
-						});
-					}
-					map.addLayer(marker);
-					marker.veloPlace = veloPlace;
-					marker.veloAddr = veloAddr;
-					marker.veloDisp = veloDisp;
-					marker.station = station;
-					marker.status = status;
-					marker.bindPopup(data[i].name + "<br>" + data[i].address);
-					// Ajout d'évenement de clique sur les marqueurs
-					marker.addEventListener("click", function(e){
-						
-						document.getElementById("form").style.display = "flex";
-						document.getElementById("formPlace").textContent = e.target.veloPlace;
-						document.getElementById("formAddr").textContent = e.target.veloAddr;
-						document.getElementById("formDisp").textContent = e.target.veloDisp;
-						document.getElementById("formStation").textContent = e.target.status + " | "  + e.target.station;
-						document.getElementById("formStation").style.fontWeight = "900";
-						document.getElementById("formDisp").style.fontWeight = "900";
-						localStorage.removeItem("station");
-						localStorage.removeItem("nom");
-						localStorage.removeItem("prenom");
-						localStorage.setItem("Station", e.target.station);
-						if (e.target.veloDisp >= 8){
-							document.getElementById("formDisp").style.color = "#00e640";
-							document.getElementById("formDisp").textContent += " (Beaucoup)";
-						} 
-						else if (e.target.veloDisp >= 5){
-							document.getElementById("formDisp").style.color = "#f9690e";
-							document.getElementById("formDisp").textContent += " (Normal)";
-						}
-						else if (e.target.veloDisp <= 4){
-							document.getElementById("formDisp").style.color = "red";
-							document.getElementById("formDisp").textContent += " (Faible)";
-
-						}
-					});
-				} 
-				else{
-					status = "FERMER";
-				}
-				
-			}
-		}
-
-		// Envoi requête
-		request.send();
-
 
 		// Lors du clique sur le bouton reserver
 		document.getElementById("formInput").addEventListener("submit", function(e){
@@ -133,5 +30,96 @@ class Maps{
 			document.getElementById("signInput").style.display = "none";
 		});
 	}
+
+	// Ajout de la carte
+	addMap()
+	{
+		this.map = L.map(this.id).setView(this.longLat, 14);
+		this.layer = L.tileLayer('https://tiles.wmflabs.org/hikebike/{z}/{x}/{y}.png', {
+			attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+		}).addTo(this.map);
+	}
+
+
+	// Récupération de l'API
+	getAjax()
+	{
+		$.getJSON('https://api.jcdecaux.com/vls/v1/stations?contract=' + this.station + '&apiKey=' + this.api_key)
+			.then((api) => {
+				this.api = api;
+				this.showMarkers();
+		});
+	}
+
+	// Création des markers
+	showMarkers()
+	{
+		this.markerCluster = L.markerClusterGroup();
+
+		for (var i = 0; i < this.api.length; i++)
+		{
+			const station = this.api[i];
+			var imageLink = "";
+
+			if (station.available_bikes >= 8)
+			{
+				imageLink = "marker_icon_green";
+			}
+			else if (station.available_bikes >= 5)
+			{
+				imageLink = "marker_icon_orange";
+			}
+			else if (station.available_bikes <= 4)
+			{
+				imageLink = "marker_icon_red";
+			}
+
+
+			const markerIcon = L.icon({
+				iconUrl: `images/${imageLink}.png`,
+				iconSize:     [38, 48]
+			});
+
+			this.marker = L.marker(station.position, {icon: markerIcon});
+			this.marker.bindPopup("<b>" + station.name + "</b><br>" + station.address);
+			this.markerCluster.addLayer(this.marker);
+			this.infoStation(station);
+		}
+
+		this.map.addLayer(this.markerCluster);
+	}
+
+	// Renvoi les informations de la station lors du clique sur le marker
+	infoStation(station) {
+		this.marker.addEventListener('click', () => {
+			this.station = station;
+			console.log(this.station);
+			document.getElementById("form").style.display = "flex";
+			document.getElementById("formPlace").textContent = this.station.bike_stands;
+			document.getElementById("formAddr").textContent = this.station.address;
+			document.getElementById("formDisp").textContent = this.station.available_bikes;
+			document.getElementById("formStation").textContent = this.station.status + " | "  + this.station.name;
+			document.getElementById("formStation").style.fontWeight = "900";
+			document.getElementById("formDisp").style.fontWeight = "900";
+			localStorage.removeItem("station");
+			localStorage.removeItem("nom");
+			localStorage.removeItem("prenom");
+			localStorage.setItem("Station", this.station.name);
+			if (this.station.available_bikes >= 8){
+				document.getElementById("formDisp").style.color = "#00e640";
+				document.getElementById("formDisp").textContent += " (Beaucoup)";
+			}
+			else if (this.station.available_bikes >= 5){
+				document.getElementById("formDisp").style.color = "#f9690e";
+				document.getElementById("formDisp").textContent += " (Normal)";
+			}
+			else if (this.station.available_bikes <= 4){
+				document.getElementById("formDisp").style.color = "red";
+				document.getElementById("formDisp").textContent += " (Faible)";
+
+			}
+		});
+	}
 }
+
 
